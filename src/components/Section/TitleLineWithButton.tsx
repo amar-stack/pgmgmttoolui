83ccd6390194fb29cf9da364db9fc0d59cef6326
unknown <amarnad.v@gmail.om>
2024-03-28 23:16:49 +0530

    changes

--- src/components/Section/TitleLineWithButton.tsx
+++ src/components/Section/TitleLineWithButton.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { mdiCog } from '@mdi/js'
 import React, { Children, ReactNode } from 'react'
 import Button from '../Button'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { mdiAccount, mdiBallotOutline, mdiHome, mdiIdCard, mdiMail, mdiNetwork, mdiPhone } from '@mdi/js'
 import { Field, Form, Formik } from 'formik'
 import Head from 'next/head'
